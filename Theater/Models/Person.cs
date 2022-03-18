a9023a0393038b0034c409090a8f0e90a13f38ac
Maurits van Scherpenseel <110689@st-maartenscollege.nl>
2022-03-18 12:33:00 +0100

    Succespagina & contactpagina laten werken.

--- Theater/Models/Person.cs
+++ Theater/Models/Person.cs
@@ -1,14 +1,19 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
-namespace Theater.Models
+namespace SendAndStore.Models
 {
     public class Person
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Phone { get; set; }
         public string Address { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
