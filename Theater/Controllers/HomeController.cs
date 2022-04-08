3337f503c0cd7e9f73f3198597a5490503e87d44
Maurits van Scherpenseel <110689@st-maartenscollege.nl>
2022-04-08 12:48:49 +0200

    Database deel 2.

--- Theater/Controllers/HomeController.cs
+++ Theater/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Theater.Models;
 using Database.Database;
+using Theater.Database;
 
 namespace Theater.Controllers
 {
@@ -24,20 +25,35 @@ namespace Theater.Controllers
 
         public IActionResult Index()
         {
-            // alle producten ophalen
+            // lijst met producten ophalen
+            var products = GetAllProducts();
+
+            // de lijst met producten in de html stoppen
+            return View(products);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            // alle producten ophalen uit de database
             var rows = DatabaseConnector.GetRows("select * from product");
 
-            // lijst maken om alle namen in te stoppen
-            List<string> names = new List<string>();
+            // lijst maken om alle producten in te stoppen
+            List<Product> products = new List<Product>();
 
             foreach (var row in rows)
             {
-                // elke naam toevoegen aan de lijst met namen
-                names.Add(row["naam"].ToString());
+                // Voor elke rij maken we nu een product
+                Product p = new Product();
+                p.Naam = row["naam"].ToString();
+                p.Prijs = row["prijs"].ToString();
+                p.Beschikbaarheid = Convert.ToInt32(row["beschikbaarheid"]);
+                p.Id = Convert.ToInt32(row["id"]);
+
+                // en dat product voegen we toe aan de lijst met producten
+                products.Add(p);
             }
 
-            // de lijst met namen in de html stoppen
-            return View(names);
+            return products;
         }
 
         [Route("titanic")]
